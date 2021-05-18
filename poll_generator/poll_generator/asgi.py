fd5e6bbaa5f081ddad7d2e4867f90f283b77f984
Renan Santana <renan_ota@hotmail.com>
2021-05-18 09:43:41 -0300

    Update asgi

--- poll_generator/poll_generator/asgi.py
+++ poll_generator/poll_generator/asgi.py
@@ -6,11 +6,9 @@ from django.core.asgi import get_asgi_application
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poll_generator.settings')
-django_asgi_app = get_asgi_application()
-
 
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
             routing.websocket_urlpatterns
