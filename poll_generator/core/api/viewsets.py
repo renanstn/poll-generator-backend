cc5c8e43ddf0955b3c3eb3157771aacda4e7ffbe
Renan Santana <renan_ota@hotmail.com>
2021-05-18 23:34:08 -0300

    Implementa endpoint de voto

--- poll_generator/core/api/viewsets.py
+++ poll_generator/core/api/viewsets.py
@@ -1,14 +1,22 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from core import models
 from core.api import serializers
-from core.websocket import consumers
 
 
 class PollViewSet(viewsets.ModelViewSet):
     queryset = models.Poll.objects.filter(active=True)
     serializer_class = serializers.PollSerializer
 
-    def create(self, request, *args, **kwargs):
-        # consumer = consumers.Consumer()
-        # consumer.send(text_data='olaaaaaaaaa')
-        return super().create(request, *args, **kwargs)
+
+class OptionViewSet(viewsets.ModelViewSet):
+    queryset = models.Option.objects.all()
+    serializer_class = serializers.OptionSerializer
+
+    @action(detail=True, methods=['get'])
+    def vote(self, request, pk=None):
+        option = self.get_object()
+        option.votes += 1
+        option.save()
+        return Response({'message': 'voto registrado'})
