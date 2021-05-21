dc1339179d2a0a15d83b8a8c1fe04d2cfcfcfd18
Renan Santana <renan_ota@hotmail.com>
2021-05-21 12:58:24 -0300

    Adiciona CORS headers

--- poll_generator/poll_generator/settings.py
+++ poll_generator/poll_generator/settings.py
@@ -16,6 +16,11 @@ SECRET_KEY = config(
 
 DEBUG = config('DEBUG', cast=bool, default=False)
 
+CORS_ALLOW_ALL_ORIGINS = config(
+    'CORS_ALLOW_ALL_ORIGINS',
+    cast=bool,
+    default=False
+)
 ALLOWED_HOSTS = []
 
 
@@ -27,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'core',
     'channels',
@@ -35,6 +41,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
